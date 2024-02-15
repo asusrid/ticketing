c76a219eba502526c2b7a1d6c3970c32152cdbdb
asusrid <alexsurid@gmail.com>
2024-02-15 16:29:23 +0100

    created 2 custom errors

--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -1,5 +1,7 @@
 import express, { Request, Response } from "express";
 import { body, validationResult } from "express-validator";
+import { RequestValidationError } from "../errors/request-validation-error";
+import { DatabaseConnectionError } from "../errors/database-connection-error";
 
 const router = express.Router();
 
@@ -15,10 +17,10 @@ router.post(
   (req: Request, res: Response) => {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
-      throw new Error("Invalid email");
+      throw new RequestValidationError(errors.array());
     }
-    // const { email, password } = req.body;
-    throw new Error("Failed connection to database");
+    console.log("Creating user...");
+    throw new DatabaseConnectionError();
 
     res.send({});
   }
