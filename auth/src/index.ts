114289f51dd934eadc543bcc52c5613278a5397f
asusrid <alexsurid@gmail.com>
2024-02-15 11:06:41 +0100

    error: router in incorrect file

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,12 +1,11 @@
 import express from 'express';
 import { json } from 'body-parser';
 
+import { currentUserRouter } from './routes/signup';
+
 const app = express();
 app.use(json());
-
-app.get('/api/users/currentuser', (req, res) => {
-    res.send('Hi there');
-})
+app.use(currentUserRouter);
 
 app.listen(3000, () => {
     console.log("Listening on port 3000!!!!!");
