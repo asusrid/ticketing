116cdcb496e0838860eeec59e55672788781da7a
asusrid <alexsurid@gmail.com>
2024-02-15 16:43:07 +0100

    middleware updated to accept new errors

--- auth/src/middlewares/error-handler.ts
+++ auth/src/middlewares/error-handler.ts
@@ -1,4 +1,6 @@
 import { Request, Response, NextFunction } from "express";
+import { RequestValidationError } from "../errors/request-validation-error";
+import { DatabaseConnectionError } from "../errors/database-connection-error";
 
 export const errorHandler = (
   err: Error,
@@ -6,7 +8,12 @@ export const errorHandler = (
   res: Response,
   next: NextFunction
 ) => {
-  console.log("Something went wrong", err);
+  if (err instanceof RequestValidationError) {
+    console.log("error req validation");
+  }
+  if (err instanceof DatabaseConnectionError) {
+    console.log("db error");
+  }
   res.status(400).send({
     message: err.message,
   });
