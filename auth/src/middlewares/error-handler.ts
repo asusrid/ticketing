9688aa637a5ed1f76fabe4a3ba717ff17e3e7d5a
asusrid <alexsurid@gmail.com>
2024-02-16 11:20:46 +0100

    reformat db error handler

--- auth/src/middlewares/error-handler.ts
+++ auth/src/middlewares/error-handler.ts
@@ -9,12 +9,18 @@ export const errorHandler = (
   next: NextFunction
 ) => {
   if (err instanceof RequestValidationError) {
-    console.log("error req validation");
+    const formattedErrors = err.errors.map((error) => {
+      // error is FieldValidationError
+      if (error.type === "field") {
+        return { message: error.msg, field: error.path };
+      }
+    });
+    return res.status(400).send({ errors: formattedErrors });
   }
   if (err instanceof DatabaseConnectionError) {
-    console.log("db error");
+    return res.status(500).send({ errors: [{ message: err.reason }] });
   }
   res.status(400).send({
-    message: err.message,
+    error: [{ message: "Something went wrong" }],
   });
 };
