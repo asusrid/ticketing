6a3a63b61e88d13d12c64740d89a1193bc500a26
asusrid <alexsurid@gmail.com>
2024-02-16 11:56:31 +0100

    normalization of errors

--- auth/src/errors/database-connection-error.ts
+++ auth/src/errors/database-connection-error.ts
@@ -1,10 +1,11 @@
-// import { ValidationError } from "express-validator";
+import { CustomError } from "./custom-error";
 
-export class DatabaseConnectionError extends Error {
+export class DatabaseConnectionError extends CustomError {
   statusCode = 500;
   reason = "Error connecting to database";
   constructor() {
-    super();
+    // only for login reasons, this wont be shown to user
+    super("Error connecting to db");
     Object.setPrototypeOf(this, DatabaseConnectionError.prototype);
   }
 
--- auth/src/errors/request-validation-error.ts
+++ auth/src/errors/request-validation-error.ts
@@ -1,9 +1,11 @@
 import { ValidationError } from "express-validator";
+import { CustomError } from "./custom-error";
 
-export class RequestValidationError extends Error {
+export class RequestValidationError extends CustomError {
   statusCode = 400;
   constructor(public errors: ValidationError[]) {
-    super();
+    // only for login reasons, this wont be shown to user
+    super("Invalid input parameters");
     // only because we are extending a built-in class
     Object.setPrototypeOf(this, RequestValidationError.prototype);
   }
@@ -13,6 +15,7 @@ export class RequestValidationError extends Error {
       if (err.type === "field") {
         return { message: err.msg, field: err.path };
       }
+      return { message: err.msg };
     });
   }
 }
